c29d17a0e8b8bb83a31dd853252035c4feb599de
zhulin <zhul5745@berryoncology.com>
2022-03-04 09:19:02 +0800

    sql表结构文件

---
+++ config/blog.sql
@@ -0,0 +1,146 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80028
+ Source Host           : localhost:3306
+ Source Schema         : blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028
+ File Encoding         : 65001
+
+ Date: 04/03/2022 09:15:01
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `categoryId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类',
+  `tagId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标签',
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '内容',
+  `thumbnail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '封面',
+  `authorId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者',
+  `hot` int(0) NULL DEFAULT NULL COMMENT '是否热门：1-是；0-否',
+  `updateTime` timestamp(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `createTime` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `hits` int(0) NULL DEFAULT NULL COMMENT '点击量',
+  `views` int(0) NULL DEFAULT NULL COMMENT '阅读量',
+  `shares` int(0) NULL DEFAULT NULL COMMENT '转发量',
+  `comments` int(0) NULL DEFAULT NULL COMMENT '评论量',
+  `collects` int(0) NULL DEFAULT NULL COMMENT '收藏量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '分类id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '评论id',
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '内容',
+  `publishTime` timestamp(0) NULL DEFAULT NULL COMMENT '发布时间',
+  `articleId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文章id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tag
+-- ----------------------------
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标签id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户id',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `birthday` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生日',
+  `sex` int(0) NULL DEFAULT NULL COMMENT '性别：0-未知；1-男；2-女',
+  PRIMARY KEY (`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Triggers structure for table article
+-- ----------------------------
+DROP TRIGGER IF EXISTS `article_id_trigger`;
+delimiter ;;
+CREATE TRIGGER `article_id_trigger` BEFORE INSERT ON `article` FOR EACH ROW begin
+    set new.id=replace(UUID(),'-','');
+end
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table category
+-- ----------------------------
+DROP TRIGGER IF EXISTS `category_id_trigger`;
+delimiter ;;
+CREATE TRIGGER `category_id_trigger` BEFORE INSERT ON `category` FOR EACH ROW begin
+    set new.id=replace(UUID(),'-','');
+end
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table comment
+-- ----------------------------
+DROP TRIGGER IF EXISTS `comment_id_trigger`;
+delimiter ;;
+CREATE TRIGGER `comment_id_trigger` BEFORE INSERT ON `comment` FOR EACH ROW begin
+    set new.id=replace(UUID(),'-','');
+end
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table tag
+-- ----------------------------
+DROP TRIGGER IF EXISTS `tag_id_trigger`;
+delimiter ;;
+CREATE TRIGGER `tag_id_trigger` BEFORE INSERT ON `tag` FOR EACH ROW begin
+    set new.id=replace(UUID(),'-','');
+end
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table user
+-- ----------------------------
+DROP TRIGGER IF EXISTS `user_id_trigger`;
+delimiter ;;
+CREATE TRIGGER `user_id_trigger` BEFORE INSERT ON `user` FOR EACH ROW begin
+    set new.id=replace(UUID(),'-','');
+end
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
